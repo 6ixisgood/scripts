2c8492db5108108b11fb4ad17ec74010c8c49046
sixisgood <17084252+sixisgoood@users.noreply.github.com>
2024-09-14 23:17:27 -0400

    update scripper

--- soundcloud-ripper/sc-ripper.py
+++ soundcloud-ripper/sc-ripper.py
@@ -22,7 +22,7 @@ class Ripper:
             widget_response = requests.get(widget_url+self.song_url)
             widget_response_text = widget_response.text
             # find the url that will get you the client ID
-            client_id_url = re.findall(r'https://widget.sndcdn.com/widget[^"]*', widget_response_text)[0]
+            client_id_url = re.findall(r'https://widget.sndcdn.com/widget[^"]*', widget_response_text)[2]
             # get that new page .js file from the link above
             client_id_response = requests.get(client_id_url)
             client_id_response_text = client_id_response.text
@@ -34,34 +34,47 @@ class Ripper:
         return self.client_id
 
     # get the json info about the song by using the api with client id generated
-    def get_json_data(self):
+    def get_json_data(self, count=0):
         if self.json_data is None:
             url = "https://api-widget.soundcloud.com/resolve?url=" + \
                   self.song_url+"&format=json&client_id=" + self.get_client_id() + "&app_version=1586353690"
-            self.json_data = (requests.get(url).json())
+            res = requests.get(url) 
+            res_json = res.json()
+            if (res.status_code == 403 or len(res_json) == 0) and count <=5:
+                # don't know what's going on here. Just keep retrying
+                return self.get_json_data(count+1) 
+            self.json_data = res_json
 
         return self.json_data
 
-    def get_mp3_data(self):
+    def get_mp3_data(self, f, count=0):
         if self.mp3_data is None:
 
             # first get the request playlist url
             playlist_request_url = self.get_json_data()['media']['transcodings'][0]['url']
             # now get the playlist link
-            playlist_url = requests.get("{}?client_id={}".format(playlist_request_url, self.get_client_id())).\
-                json()['url']
+            get_playlist_url = f"{playlist_request_url}?client_id={self.get_client_id()}"
+            get_playlist_url_res = requests.get(get_playlist_url)
+            if (get_playlist_url_res.status_code == 403 or len(get_playlist_url_res.json()) == 0) and count <=15:
+                # just retry this becaue I don't know what's going on 
+                return self.get_mp3_data(f, count+1)
+            playlist_url = get_playlist_url_res.json()['url']
             # download the pl   aylist file and parse it for the mp3 file link
             playlist_text = requests.get(playlist_url).text
             endbit = str(16*self.get_json_data()['full_duration'])
-            mp3_url = re.findall(".*https://cf-hls-media.sndcdn.com/media/0[^\n]*", playlist_text)[0]
-            mp3_url = re.sub(r'(.*/media/0)/\d+/(.*)', r'\1/{end}/\2'.format(end=endbit), mp3_url)
+            mp3_urls = re.findall(".*https://cf-hls-media.sndcdn.com/media/[^\n]*", playlist_text)
+            for u in mp3_urls:
+                content = requests.get(u).content
+                f.write(content)
+            #mp3_url = re.sub(r'(.*/media/\d+/\d+)/\d+/(.*)', r'\1/{end}/\2'.format(end=endbit), mp3_url)
 
-            self.mp3_data = requests.get(mp3_url).content
+            #self.mp3_data = requests.get(mp3_url).content
 
-        return self.mp3_data
+        return f
 
 
 def main(url, directory="."):
+    print(url, directory)
     # slice off the trailing slash
     directory = directory[:-1] if (directory[-1] == '/') else directory
     # find out if path is relative or not
@@ -72,14 +85,15 @@ def main(url, directory="."):
 
     # # get necessary mp3 info
     r = Ripper(url)
-    mp3_data = r.get_mp3_data()
+    #mp3_data = r.get_mp3_data()
     artist = r.get_json_data()['user']['username']
     title = r.get_json_data()['title']
     title_filename = title.replace(' ', '-')
     filename = "{}/{}-{}.mp3".format(directory, title_filename, artist)
     # write to file
     with open(filename, "wb+") as f:
-        f.write(r.get_mp3_data())
+        r.get_mp3_data(f)
+        #f.write(mp3_data)
 
     # get album cover
     artwork_url = r.get_json_data()['artwork_url']
