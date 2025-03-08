a1e065f76df921fc594a5196ee52a5f14be24cbf
sixisgood <17084252+sixisgoood@users.noreply.github.com>
2025-03-07 20:56:04 -0500

    update toggle scripts

--- xinput-device-toggle/toggle.sh
+++ xinput-device-toggle/toggle.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-LINE=$(xinput | grep "$1")
-ID=$(echo "$LINE" | cut -f 2 | tr 'id=' ' ')
-STATUS==$(echo "$LINE" | cut -f 3)
+DEVICES=$
 
-if [[ $STATUS  == '=[floating slave]' ]]; then
-	xinput enable $ID
-else
-	xinput disable $ID
+
+function toggle() {
+	local status=$(xinput list-props "$1" | grep "Device Enabled" | awk '{print $4}')
+
+	if [[ $status == 1 ]]; then
+		echo "Disabling $id"
+		xinput disable $id
+	else
+		echo "Enabling $id"
+		xinput enable $id
+	fi
+}
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <device_1> [device_2] [...]"
+    exit 1
 fi
+
+for device in "$@"; do
+	id=$(xinput | grep "$device" | cut -f2 | cut -d= -f2)
+    toggle "$id"
+done
